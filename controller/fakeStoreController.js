1c3b684ed77b50b10108992bd50f4845fc5f86ae
nanda <nanda@gmail.com>
2024-02-25 04:14:09 -0800

    first commit

--- controller/fakeStoreController.js
+++ controller/fakeStoreController.js
@@ -12,52 +12,10 @@ export const getStore = async (req,res) => {
 }
 
 
-
-export const createStore = async (req,res) => {
+export const getStoreById = async (req,res) => {
     try{
-        const { nama_product,gambar_product,harga,deskripsi,rating } = req.body
-        const data = await prisma.store.create({
-            data:{
-                nama_product:nama_product,
-                gambar_product:gambar_product,
-                harga:Number(harga),
-                deskripsi:deskripsi,
-                rating:Number(rating)
-            }
-        })
-        res.send(data)
-    }catch(error){
-        console.log(error)
-    }
-}
-
-export const updateStoreById = async (req,res) => {
-    try{
-        const { nama_product,gambar_product,harga,deskripsi,rating } = req.body
-        const { id } = req.params
-        const data = await prisma.store.update({
-            where:{
-                id:Number(id)
-            },
-            data:{
-                nama_product:nama_product,
-                gambar_product:gambar_product,
-                harga:Number(harga),
-                deskripsi:deskripsi,
-                rating:Number(rating)
-            }
-        })
-        res.send(data)
-    }catch(error){
-        console.log(error)
-    }
-}
-
-
-export const deleteStoreById = async (req,res) => {
-    try{
-        const { id } = req.params
-        const data = await prisma.store.delete({
+        const {id} = req.params
+        const data = await prisma.store.findUnique({
             where:{
                 id:Number(id)
             }
@@ -67,4 +25,3 @@ export const deleteStoreById = async (req,res) => {
         console.log(error)
     }
 }
-
